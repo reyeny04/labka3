ee53d2de2c61ec5b9c032b36739a3da392c7f2d8
reye <ray.so04@mail.ru>
2023-09-24 15:33:44 +0600

    ex2

---
+++ ex2/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[100000];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 100000);
+        }
+
+        StopWatch stopwatch = new StopWatch();
+        selectionSort(numbers);
+        stopwatch.stop();
+
+        System.out.println("Сортировка 100000 элементов заняло " + stopwatch.getElapsedTime() + " в милисекундах");
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
